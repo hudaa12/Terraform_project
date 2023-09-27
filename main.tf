a3e106ee840b0cc0503cc00fb8b91b867a038b88
Nebiat Mehari <nmehari25@gmail.com>
2023-09-27 17:58:00 +0100

    add access_key_2 and secret_key_2 to provider

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
   region = "eu-west-2"
   shared_credentials_files = "~/.aws/credentials"
   profile = "terraform"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.aws_access_key_2
+  secret_key = var.aws_secret_key_2
 }
 
