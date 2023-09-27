3bd43aa0d3561a5fbea2f961d89ecb26639c39ba
Huda Mohamed <Huda-mohamed1@outlook.com>
2023-09-27 14:27:57 +0100

    new code addded and old code deleted

--- main.tf
+++ main.tf
@@ -1,30 +1,13 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-
-backend "s3" {
+  backend "s3" {
     bucket = "hudneb-bucket"
-    key    = "hudneb-state"
+    key    = "terraform/dev/state.tfstate"
     region = "eu-west-2"
   }
 }
 
 provider "aws" {
   region = "eu-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-0b25f6ba2f4419235"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = var.instance_name
-  }
+  profile = "terraform"
 }
 
